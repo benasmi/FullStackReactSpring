583aefd95e338368dc3857c7320f6f6fab7540dc
pijoneris <benasmiliunas@gmail.com>
2020-03-13 01:21:01 +0200

    Cors enabled

--- src/main/java/com/springbatis/springbatisboot/SpringbatisBootApplication.java
+++ src/main/java/com/springbatis/springbatisboot/SpringbatisBootApplication.java
@@ -1,9 +1,12 @@
 package com.springbatis.springbatisboot;
 
-import org.apache.ibatis.type.MappedTypes;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @MapperScan("com.springbatis.springbatisboot.mapper")
 @SpringBootApplication
@@ -11,6 +14,16 @@ public class SpringbatisBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbatisBootApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+			}
+		};
 	}
 
 }
