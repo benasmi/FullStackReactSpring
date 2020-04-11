2b1a7d1f39279595474512fcea0929285948c10d
pijoneris <benasmiliunas@gmail.com>
2020-04-11 17:40:08 +0300

    Added ability to add multipled advanced users

--- src/main/java/com/springbatis/springbatisboot/Controllers/CardController.java
+++ src/main/java/com/springbatis/springbatisboot/Controllers/CardController.java
@@ -5,12 +5,12 @@ import com.springbatis.springbatisboot.Mappers.UsersMapper;
 import com.springbatis.springbatisboot.Models.CreditCard;
 import com.springbatis.springbatisboot.Models.User;
 import com.springbatis.springbatisboot.Models.UserWithCards;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/cards")
@@ -30,11 +30,25 @@ public class CardController {
     }
 
     @PostMapping("/insert")
-    List<CreditCard> insertCardsForUser(@RequestBody UserWithCards cardsUser){
-        usersMapper.insertUser(cardsUser.user);
-        for(int i = 0; i<cardsUser.cards.size(); i++){
-            mapper.insertCreditCard(cardsUser.user,cardsUser.cards.get(i));
+    void insertCardsForUser(@RequestBody List<UserWithCards> cardsUser){
+        for(int x = 0; x<cardsUser.size(); x++){
+            usersMapper.insertUser(cardsUser.get(x).user);
+            for(int i = 0; i<cardsUser.get(x).cards.size(); i++){
+                mapper.insertCreditCard(cardsUser.get(x).user,cardsUser.get(x).cards.get(i));
+            }
         }
-        return cardsUser.cards;
+    }
+
+    @GetMapping("/all")
+    Map<String, List<CreditCard>> getAll(){
+        List<CreditCard> cards = mapper.getAllByUser();
+        return cards.stream().collect(Collectors.groupingBy(CreditCard::getFk_email));
+    }
+
+    @PostMapping("/update")
+    void update(@RequestBody List<CreditCard> cards){
+        for (CreditCard c: cards) {
+            mapper.updateCard(c);
+        }
     }
 }
--- src/main/java/com/springbatis/springbatisboot/Mappers/CreditCardMapper.java
+++ src/main/java/com/springbatis/springbatisboot/Mappers/CreditCardMapper.java
@@ -1,12 +1,11 @@
 package com.springbatis.springbatisboot.Mappers;
 
 
+import com.springbatis.springbatisboot.Models.City;
 import com.springbatis.springbatisboot.Models.CreditCard;
 import com.springbatis.springbatisboot.Models.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import com.springbatis.springbatisboot.Models.UserWithCards;
+import org.apache.ibatis.annotations.*;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -16,6 +15,13 @@ public interface CreditCardMapper {
 
     @Select("select * from CREDIT_CARD where fk_email=#{user.email}")
     List<CreditCard> findAllForUser(@Param("user") User user);
+
+    @Select("select * from CREDIT_CARD")
+    List<CreditCard> getAllByUser();
+
+
+    @Update("UPDATE CREDIT_CARD set csv=#{card.csv}, expYear=#{card.expYear}, expMonth=#{card.expMonth} cardNumber=#{card.cardNumber} fk_cardProvider=#{card.fk_cardProvider} WHERE cardNumber=#{card.cardNumber}")
+    void updateCard(@Param("city") CreditCard card);
 
     @Insert("INSERT INTO CREDIT_CARD (csv,expYear,expMonth,cardNumber, fk_email, fk_cardProvider)" +
             "VALUES (#{card.csv}, #{card.expYear}, #{card.expMonth}, #{card.cardNumber}, #{user.email}, #{card.fk_cardProvider})")
