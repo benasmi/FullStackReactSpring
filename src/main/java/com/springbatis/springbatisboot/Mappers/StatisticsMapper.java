91dd03561c3fc1238423e40a1058a655e5ff2ae1
Benas <benasmiliunas@gmail.com>
2020-04-12 20:44:11 +0300

    stats update

--- src/main/java/com/springbatis/springbatisboot/Mappers/StatisticsMapper.java
+++ src/main/java/com/springbatis/springbatisboot/Mappers/StatisticsMapper.java
@@ -15,7 +15,7 @@ public interface StatisticsMapper {
     @Select("SELECT f.flightNumber, HOUR(TIMEDIFF(f.stopDate, f.startDate)) as hourDuration, MINUTE(TIMEDIFF(f.stopDate, f.startDate)) as minuteDuration, r.reservationId,\n" +
             "CONCAT(u.name, ' ' , u.surname) as fullName, u.email, \n" +
             "(SELECT COUNT(c.fk_email) FROM CREDIT_CARD c WHERE c.fk_email = u.email ) as totalDifCardsPerUser,\n" +
-            "(SELECT COUNT(p.fk_reservationId) FROM PAYMENT p WHERE p.fk_reservationId = r.reservationId ) as totalPaymentsPerReservation,\n" +
+            "(SELECT COUNT(*) FROM PAYMENT p WHERE p.fk_reservationId = r.reservationId ) as totalPaymentPerReservation,\n" +
             "(SELECT SUM(p.paymentAmount) FROM PAYMENT p WHERE p.fk_reservationId = r.reservationId ) as totalPaymentSumPerReservation,\n" +
             "(SELECT COUNT(DISTINCT p.fk_cardNumber) FROM PAYMENT p WHERE p.fk_reservationId = r.reservationId ) as totalDifCardsUsedPerReservation\n" +
             "FROM FLIGHT f\n" +
