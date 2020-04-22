e5c24c6c6b8485a09436757dbad433d079820e95
pijoneris <benasmiliunas@gmail.com>
2020-04-22 16:15:43 +0300

    stats payments

--- src/main/java/com/springbatis/springbatisboot/Controllers/StatisticsController.java
+++ src/main/java/com/springbatis/springbatisboot/Controllers/StatisticsController.java
@@ -1,8 +1,10 @@
 package com.springbatis.springbatisboot.Controllers;
 
 
+import com.springbatis.springbatisboot.Mappers.PaymentMapper;
 import com.springbatis.springbatisboot.Mappers.StatisticsMapper;
 import com.springbatis.springbatisboot.Models.Reservation;
+import com.springbatis.springbatisboot.Models.ReservationId;
 import com.springbatis.springbatisboot.StatiscticsModels.StatiscticsFlightsReqBody;
 import com.springbatis.springbatisboot.StatiscticsModels.StatisticsFlightResBody;
 import com.springbatis.springbatisboot.StatiscticsModels.StatisticsFlights;
@@ -19,14 +21,22 @@ public class StatisticsController {
 
 
     public StatisticsMapper mapper;
+    public PaymentMapper paymentMapper;
 
-    public StatisticsController(StatisticsMapper mapper) {
-        this.mapper = mapper;
+    public StatisticsController(StatisticsMapper mapper, PaymentMapper paymentMapper) {
+        this.mapper = mapper; this.paymentMapper = paymentMapper;
     }
 
     @PostMapping("/flights")
     Map<String, ArrayList<StatisticsFlightResBody>> getFlightStats(@RequestBody StatiscticsFlightsReqBody req){
-        List<StatisticsFlightResBody>  stats = mapper.formStatistics(req);
+        List<StatisticsFlightResBody> stats = mapper.formStatistics(req);
+        if(req.showPayments){
+            for(int i = 0; i<stats.size(); i++){
+                String k = stats.get(i).reservationId.toString();
+                stats.get(i).payments = paymentMapper.selectPaymentByReservationId(new ReservationId(Integer.parseInt(k)));
+            }
+        }
+
         Map<String, ArrayList<StatisticsFlightResBody>> rPerFlight = new HashMap<>();
         for (StatisticsFlightResBody res: stats) {
             if(!rPerFlight.containsKey(res.flightNumber)){
@@ -34,6 +44,9 @@ public class StatisticsController {
             }
             rPerFlight.get(res.flightNumber.toString()).add(res);
         }
+
+
+
         return rPerFlight;
     }
 }
